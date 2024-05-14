9bc2d9287055d5864877f093eaddfaca01ffe1db
Joseph Skinner <josephskinner7@outlook.com>
2024-05-14 13:40:37 +0100

    Lunch updates

--- Assets/Scripts/UI/Weather Generator.cs
+++ Assets/Scripts/UI/Weather Generator.cs
@@ -1,12 +1,50 @@
 using UnityEngine;
+using TMPro;
 
-public class WeatherGenerator
+public class WeatherGenerator : MonoBehaviour
 {
     public GameObject Weather;
-    public GameObject[] WeatherStates;
+    public Animator WeatherStates;
+    public string[] WeatherType;
+    public TMP_Text Text;
+    public int counter;
 
-    public void GenerateConditions()
+    public bool locked;
+
+    void Start()
     {
-     //   Weather = Random.Range(0,WeatherStates.Length - 1);
+        counter = 1;
+        Text.text = WeatherType[counter];
+    }
+
+    public void NextConditions()
+    {
+        if (locked == false)
+        {
+        WeatherStates.SetTrigger("NextState");
+
+        counter++;
+        if(counter > 4)
+        {
+            counter = 1;
+        }
+
+        Text.text = WeatherType[counter];
+
+        LockButton();
+        }
+
+    }
+
+    public void LockButton()
+    {
+        Debug.Log("Locking");
+        locked = true;
+    }
+
+    public void UnlockButton()
+    {
+        Debug.Log("Unlocking");
+        locked = false;
     }
 }
